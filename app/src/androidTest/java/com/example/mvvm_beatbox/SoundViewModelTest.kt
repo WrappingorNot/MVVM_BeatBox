01bc5c5681190dcadd22fef16c60f9e671d07a06
WrappingorNot <ksy4717@gmail.com>
2022-12-22 09:43:02 +0900

    Databinding update

--- app/src/androidTest/java/com/example/mvvm_beatbox/SoundViewModelTest.kt
+++ app/src/androidTest/java/com/example/mvvm_beatbox/SoundViewModelTest.kt
@@ -2,11 +2,18 @@ package com.example.mvvm_beatbox
 
 import junit.framework.TestCase
 import org.junit.Before
+import javax.security.auth.Subject
 
 class SoundViewModelTest : TestCase() {
+
+    private lateinit var sound: Sound
+    private lateinit var subject: SoundViewModel
 
     @Before
     public override fun setUp() {
         super.setUp()
+        sound = Sound("assetPath")
+        subject = SoundViewModel()
+        subject.sound = sound
     }
 }
